122491521d9ed9ab06011377faa10fa20754b142
Rafael Carrillo <masterxp79@gmail.com>
2012-05-09 16:07:49 -0700

    Terminando Baseline, queda pendiente el asunto de JavaMail

---
+++ sidep/src/accesoDatos/datos/DatosLibro.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesoDatos.datos;
+
+/**
+ *
+ * @author Rafuru
+ */
+public class DatosLibro {
+    
+}
---
+++ sidep/src/accesoDatos/persistencia/FachadaPersistencia.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesoDatos.persistencia;
+
+/**
+ *
+ * @author Rafuru
+ */
+public class FachadaPersistencia {
+    
+}
