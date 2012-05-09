80e73231255108d98f939279a85071928a07a0a2
Rafael Carrillo <masterxp79@gmail.com>
2012-05-09 15:36:39 -0700

    Probando el branch y actualizando el baseline

---
+++ sidep/src/negocios/SuS_AdministrarPedidos/FachadaAdministrarPedidos.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocios.SuS_AdministrarPedidos;
+
+/**
+ *
+ * @author Rafuru
+ */
+public class FachadaAdministrarPedidos {
+    
+}
